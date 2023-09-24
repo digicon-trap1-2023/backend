468151948b656f16962191725431162678b3d517
20jun01 <10junjun20@gmail.com>
2023-09-24 16:32:27 +0900

    ✨ pass db connection to ping handler

--- server/main.go
+++ server/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"github.com/digicon-trap1-2023/backend/handler"
+	"log"
 
-    "github.com/labstack/echo/v4"
+	"github.com/digicon-trap1-2023/backend/handler"
+	"github.com/digicon-trap1-2023/backend/infrastructure"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-    e := echo.New()
-	
-	api := handler.NewAPI(handler.NewPingHandler(), handler.NewAuthHandler(), handler.NewBookMarkHandler(), handler.NewDocumentHandler())
+	e := echo.New()
+
+	db, err := infrastructure.NewGormDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	api := handler.NewAPI(handler.NewPingHandler(db), handler.NewAuthHandler(), handler.NewBookMarkHandler(), handler.NewDocumentHandler())
 	handler.SetUpRouter(e, api)
 }
