422580082c27a39ebe3d06f6796e084f574b5374
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-09-25 02:39:34 +0900

    integrated TagHandler

--- server/handler/router.go
+++ server/handler/router.go
@@ -6,14 +6,22 @@ type API struct {
 	Auth     *AuthHandler
 	Bookmark *BookMarkHandler
 	Document *DocumentHandler
+	Tag      *TagHandler
 	Ping     *PingHandler
 }
 
-func NewAPI(ping *PingHandler, auth *AuthHandler, bookmark *BookMarkHandler, document *DocumentHandler) API {
+func NewAPI(
+	ping *PingHandler,
+	auth *AuthHandler,
+	bookmark *BookMarkHandler,
+	document *DocumentHandler,
+	tag *TagHandler,
+) API {
 	return API{
 		Auth:     auth,
 		Bookmark: bookmark,
 		Document: document,
+		Tag:      tag,
 		Ping:     ping,
 	}
 }
@@ -39,6 +47,12 @@ func SetUpRouter(e *echo.Echo, api API) {
 		documentsGroup.POST("", nil)
 		documentsGroup.GET("/:id", nil)
 		documentsGroup.PATCH("/:id", nil)
+	}
+
+	tagGroup := e.Group("/tags")
+	{
+		tagGroup.GET("", api.Tag.GetTags)
+		tagGroup.POST("", api.Tag.PostTag)
 	}
 
 	e.Logger.Fatal(e.Start(":8080"))
