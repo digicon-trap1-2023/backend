e8ab784ec80210e60f2d2990c10815ffce789369
20jun01 <10junjun20@gmail.com>
2023-09-25 00:37:46 +0900

    ✨ change domain to fetch response

--- server/domain/document.go
+++ server/domain/document.go
@@ -5,6 +5,17 @@ import "github.com/google/uuid"
 type Document struct {
 	Id          uuid.UUID `json:"id"`
 	Title       string    `json:"title"`
+	File        string    `json:"file"`
+	BookMarked  bool      `json:"bookMarked"`
+	Referenced  bool      `json:"referenced"`
+}
+
+type DocumentDetail struct {
+	Id          uuid.UUID `json:"id"`
+	Title       string    `json:"title"`
+	File        string    `json:"file"`
 	Description string    `json:"description"`
-	FileId      uuid.UUID `json:"fileId"`
+	Tags		[]Tag     `json:"tags"`
+	BookMarked  bool      `json:"bookMarked"`
+	Referenced  bool      `json:"referenced"`
 }
