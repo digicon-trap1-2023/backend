6f3ddad528fa0934bb3fbe0102f766cf26d7818f
20jun01 <10junjun20@gmail.com>
2023-09-25 00:38:02 +0900

    ✨ impl handler

--- server/handler/document.go
+++ server/handler/document.go
@@ -1,14 +1,38 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	"github.com/digicon-trap1-2023/backend/handler/request"
+	"github.com/digicon-trap1-2023/backend/usecases/service"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
 
 type DocumentHandler struct {
+	s *service.DocumentService
 }
 
-func NewDocumentHandler() *DocumentHandler {
-	return &DocumentHandler{}
+func NewDocumentHandler(s *service.DocumentService) *DocumentHandler {
+	return &DocumentHandler{s}
 }
 
 func (h *DocumentHandler) GetDocuments(c echo.Context) error {
-	return nil
+	var req request.GetDocumentsRequest
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	userIdString := c.Get("userId").(string)
+	userId, err := uuid.Parse(userIdString)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	documents, err := h.s.GetDocuments(userId, req.ParseTags())
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, documents)
 }
