c998fef2bc12bf22429dad9c916bd77df9819d36
20jun01 <10junjun20@gmail.com>
2023-09-24 19:08:46 +0900

    ✨ prepare sample auth middleware

--- server/handler/auth.go
+++ server/handler/auth.go
@@ -1,8 +1,24 @@
 package handler
 
+import (
+	"context"
+
+	"github.com/digicon-trap1-2023/backend/util"
+	"github.com/labstack/echo/v4"
+)
+
 type AuthHandler struct {
 }
 
 func NewAuthHandler() *AuthHandler {
 	return &AuthHandler{}
+}
+
+func (h *AuthHandler) AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := c.Request().Context()
+		newCtx := context.WithValue(ctx, util.UserKey, util.SampleUserID())
+		c.SetRequest(c.Request().WithContext(newCtx))
+		return next(c)
+	}
 }
