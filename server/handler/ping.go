260b88c27e8ee0f460478a34f55b7a9cf6c4bc3e
20jun01 <10junjun20@gmail.com>
2023-09-25 00:40:12 +0900

    ✨ handler something(:gomen:)

--- server/handler/ping.go
+++ server/handler/ping.go
@@ -4,15 +4,13 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"gorm.io/gorm"
 )
 
 type PingHandler struct {
-	conn *gorm.DB
 }
 
-func NewPingHandler(db *gorm.DB) *PingHandler {
-	return &PingHandler{db}
+func NewPingHandler() *PingHandler {
+	return &PingHandler{}
 }
 
 func (h *PingHandler) Ping(c echo.Context) error {
