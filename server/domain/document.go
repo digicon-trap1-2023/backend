32584d021b514829f8aa9e9aee4d92d2d9ae1a19
20jun01 <10junjun20@gmail.com>
2023-09-27 23:21:55 +0900

    ✨ add image size to doc

--- server/domain/document.go
+++ server/domain/document.go
@@ -7,6 +7,8 @@ type Document struct {
 	UserId         uuid.UUID `json:"userId"`
 	Title          string    `json:"title"`
 	File           string    `json:"file"`
+	FileHeight     int       `json:"file_height"`
+	FileWidth      int       `json:"file_width"`
 	Description    string    `json:"description"`
 	Tags           []*Tag    `json:"tags"`
 	BookMarked     bool      `json:"bookmarked"`
