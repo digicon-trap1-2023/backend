00c5dd90a78b369e2969d614216567ce7cf81325
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-09-28 11:28:27 +0900

    add CreatedAt to models

--- server/domain/request.go
+++ server/domain/request.go
@@ -1,16 +1,21 @@
 package domain
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Request struct {
 	Id               uuid.UUID   `json:"id"`
 	Title            string      `json:"title"`
 	Description      string      `json:"description"`
 	Tags             []uuid.UUID `json:"tags"`
-	TagNames		 []string    `json:"tag_names"`
+	TagNames         []string    `json:"tag_names"`
 	CreatedBy        uuid.UUID   `json:"created_by"`
 	CreatedUserName  string      `json:"created_user_name"`
 	RelatedDocuments []*Document `json:"related_document"`
+	CreatedAt        time.Time   `json:"created_at"`
 }
 
 func NewRequest(title string, description string, tags []uuid.UUID, createdBy uuid.UUID) *Request {
@@ -20,5 +25,6 @@ func NewRequest(title string, description string, tags []uuid.UUID, createdBy uu
 		Description: description,
 		Tags:        tags,
 		CreatedBy:   createdBy,
+		CreatedAt:   time.Now(),
 	}
 }
